ea6e32276a1433b5b9090ae82cd8687fb6ab4e4d
Jerome Laban <jerome.laban@nventive.com>
2020-05-12 11:13:12 -0400

    feat: Add supprot for CornerRadiusHelper

--- src/Uno.UI/Generated/3.0.0.0/Microsoft.UI.Xaml/CornerRadiusHelper.cs
+++ src/Uno.UI/Generated/3.0.0.0/Microsoft.UI.Xaml/CornerRadiusHelper.cs
@@ -2,24 +2,4 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Microsoft.UI.Xaml
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-	[global::Uno.NotImplemented]
-	#endif
-	public  partial class CornerRadiusHelper 
-	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public static global::Microsoft.UI.Xaml.CornerRadius FromRadii( double topLeft,  double topRight,  double bottomRight,  double bottomLeft)
-		{
-			throw new global::System.NotImplementedException("The member CornerRadius CornerRadiusHelper.FromRadii(double topLeft, double topRight, double bottomRight, double bottomLeft) is not implemented in Uno.");
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public static global::Microsoft.UI.Xaml.CornerRadius FromUniformRadius( double uniformRadius)
-		{
-			throw new global::System.NotImplementedException("The member CornerRadius CornerRadiusHelper.FromUniformRadius(double uniformRadius) is not implemented in Uno.");
-		}
-		#endif
-	}
 }
