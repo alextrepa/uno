38293a0c8410314ed2f8d7b6b0efff97b6f37dba
Jerome Laban <jerome.laban@nventive.com>
2020-05-12 07:26:38 -0400

    Adjust LottieVisualSourceProvider

--- src/AddIns/Uno.UI.Lottie/LottieVisualSourceProvider.cs
+++ src/AddIns/Uno.UI.Lottie/LottieVisualSourceProvider.cs
@@ -4,6 +4,12 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.Toolkit.Uwp.UI.Lottie;
 using Uno.Foundation.Extensibility;
 
+#pragma warning disable 105
+// Keep the space as this will not be replaced by the
+// WinUI revert tool.
+using Microsoft. UI.Xaml.Controls;
+#pragma warning restore 105
+
 [assembly: ApiExtension(typeof(ILottieVisualSourceProvider), typeof(Uno.UI.Lottie.LottieVisualSourceProvider))]
 
 namespace Uno.UI.Lottie
